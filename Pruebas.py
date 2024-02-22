93b787a59cdb9e39c88f8e959339022c381b3e81
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-02-22 22:59:54 +0000

    Subida de proyecto

--- Pruebas.py
+++ Pruebas.py
@@ -63,16 +63,24 @@ num_ships = 5
 visible_board = ""
 column_and_row = {}
 Ship_health = {}
-ship_health={}
-ship_sunked=0
-Turnos=1
+ship_health = {}
+ship_sunked = 0
+Turnos = 1
 while num_ships > 0:
-    location = input(f'Turno {Turnos}: Ataque una casilla <letra><número>: ').upper()
-    
-    if len(location) == 0 or len(location) > 3 or location[0] not in POSITION[1:] or not location[1:].isnumeric() or int(location[1:]) not in range(1, 11):
+    location = input(
+        f'Turno {Turnos}, {num_ships} barcos restantes: Ataque una casilla <letra><número>: '
+    ).upper()
+
+    if (
+        len(location) == 0
+        or len(location) > 3
+        or location[0] not in POSITION[1:]
+        or not location[1:].isnumeric()
+        or int(location[1:]) not in range(1, 11)
+    ):
         print('ERROR:La casilla que has seleccionado no se encuentra en el tablero')
         continue
-    Turnos+=1
+    Turnos += 1
     if location[0] in POSITION:
         row = int(location[1:])
         if row in column_and_row:
@@ -82,19 +90,26 @@ while num_ships > 0:
     print('     A B C D E F G H I J')
     for row in range(1, 11):
         visible_board = ""
+        num_ships_count = 0
         for column in range(1, 11):
             if row in column_and_row.keys() and column in column_and_row.get(row):
                 ship_id = board[row - 1][column - 1]
                 if ship_id != EMPTY:
                     if ship_id not in ship_health:
-                        ship_health[ship_id] = set() 
-                    ship_health[ship_id].add((column,row))
+                        ship_health[ship_id] = set()
+                    ship_health[ship_id].add((column, row))
                     if len(ship_health[ship_id]) == int(ship_id[0]):
                         visible_board += SUNKEN
+                        num_ships_count = 1
                     else:
                         visible_board += TOUCHED
+
                 else:
                     visible_board += WATER
+
             else:
                 visible_board += UNEXPLORED
+
+        num_ships -= num_ships_count
+
         print(f'{row: ^3} {visible_board}')
